f461ec0b2dab2a9f340004df3619e45a8d449113
louatiMahmoudAziz <mahmoudaziz.louati@supcom.tn>
2023-11-01 15:41:49 +0100

    add fichier2.py

---
+++ fichier2.py
@@ -0,0 +1,15 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+u = Vector(int(input("Enter x-coordinate 1: ")), int(input("Enter y-coordinate 1: ")))
+v = Vector(int(input("Enter x-coordinate 2: ")), int(input("Enter y-coordinate 2: ")))
+
+print(u + v)
